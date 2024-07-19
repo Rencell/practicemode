29fd1076857d447525eb16aeeb7b5c48eff4dbaa
rencellgitggggasdgggggsdasdassdc <rencelltobelonia67@gmail.com>
2024-07-20 02:15:40 +0800

    Add project files.

---
+++ practicemode/Form1.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace practicemode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string connectionString = "Server=.\\SQLEXPRESS;Database=mynigga;Integrated Security=True;Encrypt=True;TrustServerCertificate=True;";
+
+
+
+            SqlConnection con = new SqlConnection(connectionString);
+
+            try
+            {
+                con.Open();
+                string query = "SELECT username FROM users WHERE id = 1";
+
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    string userName = (string)cmd.ExecuteScalar();
+
+                    textBox1.Text = userName;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error executing SQL query: {ex.Message}");
+            }
+        }
+    }
+}
